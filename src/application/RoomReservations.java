b105c0f7afd3f25f9ce2e01779ff730347fc4dbf
bertuzzij <jhonatanbertuzzi@gmail.com>
2020-04-10 15:35:25 -0300

    finalização do projeto para criar reserva de quarto

--- src/application/RoomReservations.java
+++ src/application/RoomReservations.java
@@ -1,33 +1,50 @@
 package application;
 
 import entities.Guest;
-
 import java.util.Scanner;
 
 public class RoomReservations {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 
-        System.out.printf("How many rooms will be rented? ");
-        int rooms = scan.nextInt();
+		Guest[] vect = new Guest[10];// instanciado um vetor de 10 posições nulos
 
-        Guest[] vect = new Guest[rooms];
+		System.out.printf("How many rooms will be rented? ");
+		int rooms = scan.nextInt();
 
-        for (int i = 0; i < vect.length; i++){
-            scan.nextLine();
-            System.out.printf("Insert a name for the reservation: ");
-            String name = scan.nextLine();
-            System.out.printf("Insert a email: ");
-            String email = scan.nextLine();
-            System.out.printf("Select your room (1 -10): ");
-            int room = scan.nextInt();
-            vect[i] = new Guest(name, room, email);
+		for (int i = 1; i <= rooms; i++) {
+			scan.nextLine();
+			System.out.printf("Rent #" + i);
+			System.out.printf("%nInsert a name for the reservation: ");
+			String name = scan.nextLine();
 
-            //vect[i].getRoom();
-        }
+			System.out.printf("Insert a email: ");
+			String email = scan.nextLine();
 
-        System.out.println();
+			System.out.printf("Select your room (1 -10): ");
+			int room = scan.nextInt();
 
-        scan.close();
-    }
+			vect[room] = new Guest(name, email);// instanciado um vetor a partir dos quartos reservados
+		}
+
+		System.out.printf("%nBusy rooms: %n");
+		for (int i = 0; i < 10; i++) {
+			if (vect[i] != null) {
+				System.out.println(i + ": " + vect[i]);
+			}
+
+// Marcação para eu não esquecer esse raciocínio {
+//        String reservation = "";
+//        for (int i = 0; i < n; i++) {
+//        reservation = vect[i].getName()
+//        			+" , E-mail: "+ vect[i].getEmail()
+//					+" , Room: "+room;
+//        
+//        System.out.printf(reservation+"%n");
+//        }
+//		}
+
+			scan.close();
+		}
+	}
 }
--- src/entities/Guest.java
+++ src/entities/Guest.java
@@ -1,14 +1,15 @@
 package entities;
 
 public class Guest {
+	
     private String name;
-    private int room;
     private String email;
+    
 
-    public Guest(String name, int room, String email) {
+    public Guest(String name, String email) {
         this.name = name;
-        this.room = room;
         this.email = email;
+        
     }
 
     public String getName() {
@@ -17,14 +18,6 @@ public class Guest {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public int getRoom() {
-        return room;
-    }
-
-    public void setRoom(int room) {
-        this.room = room;
     }
 
     public String getEmail() {
@@ -37,10 +30,9 @@ public class Guest {
 
     public String toString(){
         return "Name: " + name
-                +"Email: "
-                +email
-                +"Room: "
-                +room;
+                +", "
+                +email;
+
     }
 
 }
